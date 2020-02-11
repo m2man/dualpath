750219d422bad9893f5fe1d9073fd75f58724290
duydedai <nmduy881994@gmail.com>
2020-02-11 16:36:41 +0000

    Update config_dualpath.py

--- config_dualpath.py
+++ config_dualpath.py
@@ -1,8 +1,8 @@
 dictionary_path = 'Flickr30k_dictionary.pickle'
 dataset_path = 'Flickr30k_dataset.pickle'
-numb_epochs = 11
+numb_epochs = 50
 seeds = [x for x in range(numb_epochs)]
-batch_size = 32
+batch_size = 16 #16
 #learn_rate = 0.01
 #moment_val = 0.9
 
@@ -18,3 +18,7 @@ lamb_1 = 1
 lamb_2 = 1
 
 kernel_init = True # Only set true if run from sratch (if load pretrained --> should make false for faster loading)
+
+
+if stage_2 == False:
+	lamb_0 = 0
